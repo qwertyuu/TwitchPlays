10a3fc5fe53d2b80b20644b19fb50bfc1fcadb01
Raphaël Côté <cotlarrc@msn.com>
2014-02-24 00:17:35 -0500

    Initial!

---
+++ Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace TwitchPlays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.left = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // left
+            // 
+            this.left.Location = new System.Drawing.Point(32, 77);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(75, 23);
+            this.left.TabIndex = 0;
+            this.left.Text = "button1";
+            this.left.UseVisualStyleBackColor = true;
+            this.left.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.left.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button4_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(95, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button4_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(167, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button2";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button3_MouseDown);
+            this.button3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button4_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(95, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "button2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button4_MouseDown);
+            this.button4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button4_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.left);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using vJoyInterfaceWrap;
+
+namespace TwitchPlays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            joystick = new vJoy();
+            id = 1;
+            ///// Write access to vJoy Device - Basic
+            VjdStat status;
+            status = joystick.GetVJDStatus(id);
+            // Acquire the target
+            string prt;
+            if ((status == VjdStat.VJD_STAT_OWN) ||
+            ((status == VjdStat.VJD_STAT_FREE) && (!joystick.AcquireVJD(id))))
+                prt = String.Format("Failed to acquire vJoy device number {0}.", id);
+            else
+                prt = String.Format("Acquired: vJoy device number {0}.", id);
+            Reset();
+
+            //MessageBox.Show(prt);
+        }
+        vJoy joystick;
+        uint id;
+
+        private void Reset()
+        {
+            foreach (var item in (HID_USAGES[]) Enum.GetValues(typeof(HID_USAGES)))
+            {
+                var lel = joystick.SetAxis(16500, id, item);
+            }
+
+        }
+
+        private void button4_MouseUp(object sender, MouseEventArgs e)
+        {
+            Reset();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            joystick.SetAxis(0, id, HID_USAGES.HID_USAGE_X);
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            joystick.SetAxis(0, id, HID_USAGES.HID_USAGE_Y);
+        }
+
+        private void button3_MouseDown(object sender, MouseEventArgs e)
+        {
+            joystick.SetAxis(33000, id, HID_USAGES.HID_USAGE_X);
+        }
+
+        private void button4_MouseDown(object sender, MouseEventArgs e)
+        {
+            joystick.SetAxis(33000, id, HID_USAGES.HID_USAGE_Y);
+        }
+    }
+}
